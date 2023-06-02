4979740d0a567643ed99a076b7c20e1cfa0726d9
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-06-02 14:48:52 -0400

    inital commit

---
+++ src/main/java/com/codeup/pawspursuit/PawsPursuitApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.pawspursuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsPursuitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawsPursuitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/pawspursuit/PawsPursuitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.pawspursuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsPursuitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
