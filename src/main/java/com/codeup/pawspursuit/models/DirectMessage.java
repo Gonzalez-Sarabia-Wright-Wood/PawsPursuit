5693e280cc08b77b8de0e8b4a98a668908935744
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-06-02 19:34:37 -0400

    direct messages model complete

---
+++ src/main/java/com/codeup/pawspursuit/models/DirectMessage.java
@@ -0,0 +1,58 @@
+package com.codeup.pawspursuit.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "direct_message")
+public class DirectMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    @Column(name = "message")
+    private String message;
+
+    public DirectMessage() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
