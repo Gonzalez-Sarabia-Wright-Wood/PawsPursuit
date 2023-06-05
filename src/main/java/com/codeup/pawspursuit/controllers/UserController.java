1db2a550a1a87db12f37355ccfcf6887493d483f
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-06-05 16:44:03 -0400

    updated endpoints

--- src/main/java/com/codeup/pawspursuit/controllers/UserController.java
+++ src/main/java/com/codeup/pawspursuit/controllers/UserController.java
@@ -8,69 +8,70 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 @Controller
-public class UserController{
+public class UserController {
 
     private UserRepository userDao;
 
-    public UserController(UserRepository userDao){
+    public UserController(UserRepository userDao) {
         this.userDao = userDao;
     }
 
     @GetMapping("/login")
-    public String userLoginGet(){
+    public String userLoginGet() {
         return "/login";
     }
+
     @PostMapping("/login")
-    public String userLoginPost(@RequestParam String username, Model model){
+    public String userLoginPost(@RequestParam String username, Model model) {
         User user = userDao.findUserByUsername(username);
         model.addAttribute("user", user);
         return "redirect:/profile/" + user.getId();
     }
 
     @GetMapping("/register")
-    public String showRegisterForm(Model model){
+    public String showRegisterForm(Model model) {
         model.addAttribute("user", new User());
         return "/register";
     }
 
     @PostMapping("/register")
-    public String saveUser(@ModelAttribute User user){
+    public String saveUser(@ModelAttribute User user) {
         userDao.save(user);
         return "redirect:/login";
     }
 
     @GetMapping("/profile/{id}")
-    public String viewProfile(Model model,@PathVariable Long id){
+    public String viewProfile(Model model, @PathVariable Long id) {
         User user = userDao.findById(id).get();
         model.addAttribute("user", user);
         return "User/profile";
     }
 
-    @GetMapping(path = "/User/{id}/edit")
+    @GetMapping(path = "/profile/{id}/edit")
     public String editUser(Model model, @PathVariable Long id) {
         User user = userDao.findById(id).get();
         model.addAttribute("user", user);
         return "User/edit";
     }
 
-    @PostMapping("/User/{id}/delete")
-    public String deleteUserPost(@RequestParam Long id){
+    @PostMapping("/profile/{id}/delete")
+    public String deleteUserPost(@RequestParam Long id) {
         userDao.deleteById(id);
         return "redirect:/login";
     }
 
-    @GetMapping("/User/chat")
-    public String showChat(Model model, @PathVariable Long id){
+    @GetMapping("/chat")
+    public String showChat(Model model, @PathVariable Long id) {
         return "User/chat";
     }
 
-    @PostMapping("/User/chat")
-    public String sendChat(){
+    @PostMapping("/chat")
+    public String sendChat() {
         return "User/chat";
     }
 
-    @GetMapping("/User/messages")
-    public String showAllMessages(){
+    @GetMapping("/profile/messages")
+    public String showAllMessages() {
         return "User/messages";
     }
 }
