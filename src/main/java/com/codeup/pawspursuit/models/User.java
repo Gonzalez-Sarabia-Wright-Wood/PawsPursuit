bca1dd43b243b674620870ff306387ed721b7f05
Genesis Sarabia <genesissarabia14@gmail.com>
2023-06-07 11:16:51 -0500

    beginning implementation of chat portion with TalkJS api. Currently able to connect to database, working on communicating between users

--- src/main/java/com/codeup/pawspursuit/models/User.java
+++ src/main/java/com/codeup/pawspursuit/models/User.java
@@ -135,4 +135,8 @@ public class User {
     public void setPosts(List<Post> posts) {
         this.posts = posts;
     }
+
+    public String getName(){
+        return firstName+ " " +lastName;
+    }
 }
