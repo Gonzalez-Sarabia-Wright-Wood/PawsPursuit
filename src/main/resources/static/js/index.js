77dc75dcc7b0683d641aac183236fb7b6f2ce919
RileyWood9984 <rjw9984@gmail.com>
2023-06-12 20:34:05 -0500

    broke

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,14 +3,21 @@ for(let i=0;i<lost.length;i++){
     if(lost[i].innerHTML==='false'){
         lost[i].innerHTML= "Lost";
     }else{
-        lost[i].innerHTML="Reunited"
+        lost[i].innerHTML="Reunited";
     }
 }
-let modal = document.getElementsByClassName("modal-label");
-let toEdit = document.getElementsByClassName("to-edit")
-for(let i =0; i<modal.length;i++){
-    let part1 = toEdit[i].outerHTML
-    part1+=" "
-    part1.shift()
-    modal[i].innerHTML
+let modal = document.getElementsByClassName("ml");
+let toModal = document.getElementsByClassName("tm");
+let edit = document.getElementsByClassName("te");
+
+for(let i =0; i<=modal.length;i++){
+    let a = Array.from(toModal[i].outerHTML);
+    a.splice(98,1, "modal"+modal[i].innerHTML);
+    a=a.join("");
+    toModal[i].outerHTML=a;
+
+    let b =Array.from(edit[i].outerHTML);
+    b.splice(10,1,"modal"+modal[i].innerHTML);
+    b=b.join("");
+    edit[i].outerHTML=b;
 }
