b3a1dc2d45c2e1c62e0d3ea4f566380d5fbc3f6b
RileyWood9984 <rjw9984@gmail.com>
2023-06-13 11:55:57 -0500

    qs

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,18 +6,3 @@ for(let i=0;i<lost.length;i++){
         lost[i].innerHTML="Reunited";
     }
 }
-let modal = document.getElementsByClassName("ml");
-let toModal = document.getElementsByClassName("tm");
-let edit = document.getElementsByClassName("te");
-
-for(let i =0; i<=modal.length;i++){
-    let a = Array.from(toModal[i].outerHTML);
-    a.splice(98,1, "modal"+modal[i].innerHTML);
-    a=a.join("");
-    toModal[i].outerHTML=a;
-
-    let b =Array.from(edit[i].outerHTML);
-    b.splice(10,1,"modal"+modal[i].innerHTML);
-    b=b.join("");
-    edit[i].outerHTML=b;
-}
